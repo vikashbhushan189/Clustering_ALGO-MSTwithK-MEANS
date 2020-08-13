6b8a1712889d88e8342b749a105e08b449a3b4ba
vikashbhushan189 <30977392+vikashbhushan189@users.noreply.github.com>
2020-08-13 12:37:59 +0530

    Update untitled2.py

--- K_Means clutering and extended using MST clustering/untitled2.py
+++ K_Means clutering and extended using MST clustering/untitled2.py
@@ -22,38 +22,9 @@ def plot_mst(model,marker='s', cmap='rainbow'):
     ax[1].set_title('Trimmed Minimum Spanning Tree', size=16);
     
     
-
 # create some data
 X, y = make_blobs(100, centers=5, cluster_std=0.90)
 print(X)
-print(y)
-
-
-
-plt.scatter(X[:,0], X[:,1])
-
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
-plt.plot(range(1, 11), wcss)
-plt.title('Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
-
-kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_state=0)
-pred_y = kmeans.fit_predict(X)
-counts = np.bincount(pred_y)
-print("No. of clusters: ")
-print(counts.shape)
-print("No. of elements in each Clusters: ")
-print(counts)
-plt.scatter(X[:,0], X[:,1])
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=100, c='red')
-plt.show()
-
 
 
 # predict the labels with the MST algorithm
@@ -61,7 +32,8 @@ model = MSTClustering(cutoff_scale=1.5, approximate=True, n_neighbors=100)
 labels = model.fit_predict(X)
 counts = np.bincount(labels)
 print("No. of clusters: ")
-print(counts.shape)
+clusters=len(counts)
+print(len(counts))
 print("No. of elements in each Clusters: ")
 print(counts)
 
@@ -70,3 +42,24 @@ plt.scatter(X[0:, 0], X[0:, 1],marker='o', c=labels, cmap='rainbow');
 plt.show()
 # plot the brief model
 plot_mst(model)
+
+
+
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+
+plt.show()
+
+kmeans = KMeans(n_clusters=clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+pred_y = kmeans.fit_predict(X)
+counts = np.bincount(pred_y)
+print("No. of clusters: ")
+print(len(counts))
+print("No. of elements in each Clusters: ")
+print(counts)
+plt.scatter(X[:,0], X[:,1],marker='o',c=pred_y, cmap='rainbow')
+plt.show()
